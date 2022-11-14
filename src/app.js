73cd4ac575a4cbf93a560779cd610f39a50e1ae4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-11-14 17:36:05 -0300

    add GET `/missions` route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const { readMissionsData } = require('./utils/fsUtils');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/missions', async (request, response) => {
+  const missions = await readMissionsData();
+  return response.json({ missions });
+});
 
 module.exports = app;
